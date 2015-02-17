5d0037093c610b1359b10eb8680025e57d18d07c
Casey Ward <caseyward1@gmail.com>
2015-02-16 20:17:37 -0800

    Completed strings

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,32 +12,33 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    NSString *realCheeseName = cheeseName;
-    NSString *fav = [NSString stringWithFormat:@"My favorite cheese is %@", realCheeseName];
-    return fav;
+    return [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     /* WORK HERE */
-    // I believe this wasn't the intended method to use for this checkpoint.
-    return cheeseName = [cheeseName stringByReplacingOccurrencesOfString:@" cheese"
-                                                           withString:@""];
+   // NSRange findTheWordCheese = [cheeseName rangeOfString:@"cheese" options:NSCaseInsensitiveSearch];
+    NSString *noCheeseStatement = [cheeseName stringByReplacingOccurrencesOfString:@" cheese"
+                                                                        withString:@""
+                                                                           options:NSCaseInsensitiveSearch
+                                                                             range:NSMakeRange(0, cheeseName.length)];
+    return noCheeseStatement;
 }
-
 
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     if (cheeseCount == 1) {
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        return @"1 cheese";
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        return [NSString stringWithFormat:@"%lu cheeses", (unsigned long)cheeseCount];
     }
     
     /*
      (You will learn more about if/else statements in the next checkpoint.)
      */
     
-    return nil;
 }
 
 @end
